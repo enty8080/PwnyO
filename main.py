1a7aa0d91a6eb06f16ec33e69f5b24af50709c46
Ivan Nikolsky <enty8080@gmail.com>
2023-07-08 19:13:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import socket
 
+def send(c, tag, data):
+    c.send(tag.to_bytes(2, 'little'))
+    c.send(len(data).to_bytes(4, 'little'))
+    c.send(data.encode())
+
+def read(c):
+    tag = c.recv(2)
+    size = c.recv(4)
+
+    tag = int.from_bytes(tag, 'little')
+    size = int.from_bytes(size, 'little')
+
+    if size > 0:
+        return tag, c.recv(size)
+
+    return tag, b''
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
 s.bind(('127.0.0.1', 8888))
 s.listen()
@@ -7,11 +24,4 @@ c, a = s.accept()
 
 print(f"* Connection from {a[0]}")
 
-data = input('> ')
-
-tag = 0
-size = len(data)
-
-c.send(tag.to_bytes(2, 'little'))
-c.send(size.to_bytes(4, 'little'))
-c.send(data.encode())
+send(c, 0, 'hi')
